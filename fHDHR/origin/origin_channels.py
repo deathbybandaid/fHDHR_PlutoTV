5c9cbb0c6b7071a3b5ec012820eb7bf2dae46846
deathbybandaid <github@deathbybandaid.net>
2020-10-21 10:40:30 -0400

    Update Core

---
+++ fHDHR/origin/origin_channels.py
@@ -0,0 +1,128 @@
+import json
+import urllib.parse
+import m3u8
+
+import fHDHR.tools
+
+
+class OriginService():
+
+    def __init__(self, settings):
+        self.config = settings
+
+        self.web = fHDHR.tools.WebReq()
+
+        self.base_api_url = 'https://api.pluto.tv'
+        self.login_url = self.base_api_url + '/v1/auth/local'
+
+        self.token = None
+        self.userid = None
+
+        self.login()
+
+    def login(self):
+        print("Logging into PlutoTV")
+        if (not self.config.dict["origin"]["username"] or not self.config.dict["origin"]["password"]):
+            print("No Username/Password set, will operate in Guest Mode.")
+            return True
+
+        form_data = {
+                      'optIn': 'true',
+                      'password': self.config.dict["origin"]["password"],
+                      'synced': 'false',
+                      'email': self.config.dict["origin"]["username"]
+                      }
+        try:
+            loginreq = self.web.session.post(self.login_url, data=form_data)
+            loginresp = json.loads(loginreq.content)
+            if "accessToken" not in list(loginresp.keys()):
+                print("Login Failed, will use Guest Mode.")
+                return True
+            print("Login Success!")
+        except Exception as e:
+            print("Login Failed, will use Guest Mode. " + str(e))
+            return True
+        self.userid = loginresp["_id"]
+        self.token = loginresp["accessToken"]
+        return True
+
+    def get_channels(self):
+
+        url = self.base_api_url + "/v2/channels.json"
+        urlopn = self.web.session.get(url)
+        pluto_chan_list = urlopn.json()
+
+        channel_list = []
+        for channel_dict in pluto_chan_list:
+
+            if (channel_dict["isStitched"]
+               and channel_dict["visibility"] in ["everyone"]
+               and not channel_dict['onDemand']):
+
+                clean_station_item = {
+                                     "name": channel_dict["name"],
+                                     "callsign": channel_dict["name"],
+                                     "number": str(channel_dict["number"]),
+                                     "id": str(channel_dict["_id"]),
+                                     }
+                channel_list.append(clean_station_item)
+        return channel_list
+
+    def get_channel_stream(self, chandict, allchandict):
+        caching = True
+        streamlist = []
+        streamdict = {}
+
+        url = self.base_api_url + "/v2/channels.json"
+        urlopn = self.web.session.get(url)
+        pluto_chan_list = urlopn.json()
+        pluto_chandict = self.get_channel_dict_pluto(pluto_chan_list, "_id", chandict["id"])
+
+        streamurl = pluto_chandict["stitched"]["urls"][0]["url"]
+        streamurl = self.channel_stream_url_cleanup(streamurl)
+        if self.config.dict["origin"]["force_best"]:
+            streamurl = self.m3u8_beststream(streamurl)
+        streamdict = {"number": str(chandict["number"]), "stream_url": streamurl}
+        streamlist.append(streamdict)
+        return streamlist, caching
+
+    def get_channel_dict_pluto(self, chanlist, keyfind, valfind):
+        return next(item for item in chanlist if item[keyfind] == valfind)
+
+    def channel_stream_url_cleanup(self, streamurl):
+
+        streamurl = streamurl.replace("\\u0026", "&")
+        streamurl_base = streamurl.split("?")[0]
+        streamurl_params = streamurl.split("?")[1].split("&")
+
+        paramdict = {}
+
+        for param in streamurl_params:
+            paramkey = param.split("=")[0]
+            paramval = param.split("=")[1]
+            paramdict[paramkey] = paramval
+
+        paramdict["deviceMake"] = "Chrome"
+        paramdict["deviceType"] = "web"
+        paramdict["deviceModel"] = "Chrome"
+        paramdict["sid"] = self.config.dict["main"]["uuid"]
+        paramdict["userId"] = self.userid or ''
+
+        return streamurl_base + "?" + urllib.parse.urlencode(paramdict)
+
+    def m3u8_beststream(self, m3u8_url):
+        bestStream = None
+        videoUrlM3u = m3u8.load(m3u8_url)
+        if not videoUrlM3u.is_variant:
+            return m3u8_url
+
+        for videoStream in videoUrlM3u.playlists:
+            if not bestStream:
+                bestStream = videoStream
+            elif videoStream.stream_info.bandwidth > bestStream.stream_info.bandwidth:
+                bestStream = videoStream
+
+        if not bestStream:
+            return bestStream.absolute_uri
+        else:
+            return m3u8_url
--- fHDHR/origin/origin_epg.py
+++ fHDHR/origin/origin_epg.py
@@ -1,90 +1,29 @@
 import time
-import json
 import datetime
-import urllib.parse
-import m3u8
+import json
 
 import fHDHR.tools
 
 
-class fHDHRservice():
-    def __init__(self, settings):
+class originEPG():
+
+    def __init__(self, settings, channels):
         self.config = settings
+        self.channels = channels
 
         self.web = fHDHR.tools.WebReq()
 
         self.base_api_url = 'https://api.pluto.tv'
-        self.login_url = self.base_api_url + '/v1/auth/local'
         self.web_cache_dir = self.config.dict["filedir"]["epg_cache"]["origin"]["web_cache"]
 
-        self.token = None
-        self.userid = None
+    def xmltimestamp_pluto(self, inputtime):
+        xmltime = inputtime.replace('Z', '+00:00')
+        xmltime = datetime.datetime.fromisoformat(xmltime)
+        xmltime = xmltime.strftime('%Y%m%d%H%M%S %z')
+        return xmltime
 
-    def login(self):
-        print("Logging into PlutoTV")
-        if (not self.config.dict["origin"]["username"] or not self.config.dict["origin"]["password"]):
-            print("No Username/Password set, will operate in Guest Mode.")
-            return True
-
-        form_data = {
-                      'optIn': 'true',
-                      'password': self.config.dict["origin"]["password"],
-                      'synced': 'false',
-                      'email': self.config.dict["origin"]["username"]
-                      }
-        try:
-            loginreq = self.web.session.post(self.login_url, data=form_data)
-            loginresp = json.loads(loginreq.content)
-            if "accessToken" not in list(loginresp.keys()):
-                print("Login Failed, will use Guest Mode.")
-                return True
-            print("Login Success!")
-        except Exception as e:
-            print("Login Failed, will use Guest Mode. " + str(e))
-            return True
-        self.userid = loginresp["_id"]
-        self.token = loginresp["accessToken"]
-        return True
-
-    def get_channels(self):
-
-        url = self.base_api_url + "/v2/channels.json"
-        urlopn = self.web.session.get(url)
-        pluto_chan_list = urlopn.json()
-
-        channel_list = []
-        for channel_dict in pluto_chan_list:
-
-            if (channel_dict["isStitched"]
-               and channel_dict["visibility"] in ["everyone"]
-               and not channel_dict['onDemand']):
-
-                clean_station_item = {
-                                     "name": channel_dict["name"],
-                                     "callsign": channel_dict["name"],
-                                     "number": str(channel_dict["number"]),
-                                     "id": str(channel_dict["_id"]),
-                                     }
-                channel_list.append(clean_station_item)
-        return channel_list
-
-    def get_channel_stream(self, chandict, allchandict):
-        caching = True
-        streamlist = []
-        streamdict = {}
-
-        url = self.base_api_url + "/v2/channels.json"
-        urlopn = self.web.session.get(url)
-        pluto_chan_list = urlopn.json()
-        pluto_chandict = self.get_channel_dict_pluto(pluto_chan_list, "_id", chandict["id"])
-
-        streamurl = pluto_chandict["stitched"]["urls"][0]["url"]
-        streamurl = self.channel_stream_url_cleanup(streamurl)
-        if self.config.dict["origin"]["force_best"]:
-            streamurl = self.m3u8_beststream(streamurl)
-        streamdict = {"number": str(chandict["number"]), "stream_url": streamurl}
-        streamlist.append(streamdict)
-        return streamlist, caching
+    def duration_pluto_minutes(self, induration):
+        return ((int(induration))/1000/60)
 
     def get_channel_thumbnail(self, channel_id):
         channel_thumb_url = ("http://images.pluto.tv/channels/%s/logo.png" % str(channel_id))
@@ -93,47 +32,6 @@ class fHDHRservice():
     def get_content_thumbnail(self, content_id):
         item_thumb_url = ("https://images.pluto.tv/episodes/%s/poster.jpg" % str(content_id))
         return item_thumb_url
-
-    def get_channel_dict_pluto(self, chanlist, keyfind, valfind):
-        return next(item for item in chanlist if item[keyfind] == valfind)
-
-    def channel_stream_url_cleanup(self, streamurl):
-
-        streamurl = streamurl.replace("\\u0026", "&")
-        streamurl_base = streamurl.split("?")[0]
-        streamurl_params = streamurl.split("?")[1].split("&")
-
-        paramdict = {}
-
-        for param in streamurl_params:
-            paramkey = param.split("=")[0]
-            paramval = param.split("=")[1]
-            paramdict[paramkey] = paramval
-
-        paramdict["deviceMake"] = "Chrome"
-        paramdict["deviceType"] = "web"
-        paramdict["deviceModel"] = "Chrome"
-        paramdict["sid"] = self.config.dict["main"]["uuid"]
-        paramdict["userId"] = self.userid or ''
-
-        return streamurl_base + "?" + urllib.parse.urlencode(paramdict)
-
-    def m3u8_beststream(self, m3u8_url):
-        bestStream = None
-        videoUrlM3u = m3u8.load(m3u8_url)
-        if not videoUrlM3u.is_variant:
-            return m3u8_url
-
-        for videoStream in videoUrlM3u.playlists:
-            if not bestStream:
-                bestStream = videoStream
-            elif videoStream.stream_info.bandwidth > bestStream.stream_info.bandwidth:
-                bestStream = videoStream
-
-        if not bestStream:
-            return bestStream.absolute_uri
-        else:
-            return m3u8_url
 
     def update_epg(self):
         programguide = {}
@@ -229,12 +127,3 @@ class fHDHRservice():
                 pass
             print('Removing stale cache file:', p.name)
             p.unlink()
-
-    def xmltimestamp_pluto(self, inputtime):
-        xmltime = inputtime.replace('Z', '+00:00')
-        xmltime = datetime.datetime.fromisoformat(xmltime)
-        xmltime = xmltime.strftime('%Y%m%d%H%M%S %z')
-        return xmltime
-
-    def duration_pluto_minutes(self, induration):
-        return ((int(induration))/1000/60)
